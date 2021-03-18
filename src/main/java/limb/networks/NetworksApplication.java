fd6138ce5ef4e35cfe8f2c3f5709f0cb27700fcc
igor_kulikov <gah_kulikov@mail.ru>
2021-03-18 17:27:30 +0300

    init

---
+++ src/main/java/limb/networks/NetworksApplication.java
@@ -0,0 +1,13 @@
+package limb.networks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/limb/networks/NetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package limb.networks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
